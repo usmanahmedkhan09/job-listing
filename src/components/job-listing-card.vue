38fb45482dd8b2d20574348c640cddce1241666a
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-26 23:57:48 +0500

    changes in card

--- src/components/job-listing-card.vue
+++ src/components/job-listing-card.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="jobsCard"></div>
+  <div class="jobsCard">
+    <img :src="getImageUrl(job?.logo)" alt="" />
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  props: {
+    job: {
+      type: Object,
+    },
+  },
   setup() {
-    return {};
+    const getImageUrl = (name: any) => {
+      return new URL(`../assets/images/${name}.svg`, import.meta.url).href;
+    };
+    return { getImageUrl };
   },
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,19 @@
     <!-- <div class="header__autocomplete" contenteditable="true"></div> -->
   </header>
   <main class="content">
-    <jobListingCardVue v-for="item in [1, 2, 3, 4, 4]" :key="item" />
+    <jobListingCardVue v-for="(item, index) in jobs" :key="index" :job="item" />
   </main>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import jobListingCardVue from "@/components/job-listing-card.vue";
+import jobsData from "../assets/data.json";
 
 export default defineComponent({
   components: { jobListingCardVue },
   setup() {
-    return {};
+    const jobs = ref(jobsData);
+    return { jobs };
   },
 });
 </script>
