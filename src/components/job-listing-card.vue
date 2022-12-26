64523a410264d680ba675e90f6d94b04c7f3b501
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-26 23:40:52 +0500

    job listing card is added

---
+++ src/components/job-listing-card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="jobsCard"></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
   <header class="header">
-    <div class="header__autocomplete" contenteditable="true"></div>
+    <!-- <div class="header__autocomplete" contenteditable="true"></div> -->
   </header>
-  <main class="content"></main>
+  <main class="content">
+    <jobListingCardVue v-for="item in [1, 2, 3, 4, 4]" :key="item" />
+  </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import jobListingCardVue from "@/components/job-listing-card.vue";
 
 export default defineComponent({
+  components: { jobListingCardVue },
   setup() {
     return {};
   },
