49108762373629d6323fa8e009cf67fd17d2a8e4
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-26 22:02:16 +0500

    changes in css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+  <header class="header"></header>
+  <main></main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
