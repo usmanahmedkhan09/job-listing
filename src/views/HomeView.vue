6a7200d88274ae51b63f1ab46374476b203b519b
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-29 20:07:44 +0500

    autocomplete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,30 @@
 <template>
   <header class="header">
-    <!-- <div class="header__autocomplete" contenteditable="true"></div> -->
+    <div class="header__autocomplete">
+      <div
+        class="header__autocomplete--filters"
+        v-for="(item, index) in selectfilters"
+        :key="index"
+      >
+        <span class="keyword">{{ item }}</span>
+        <div class="icon">
+          <img src="../assets/images/icon-remove.svg" alt="" />
+        </div>
+      </div>
+      <input
+        v-model="keyword"
+        class="input"
+        type="text"
+        @keypress.enter="handleKeyword($event)"
+      />
+      <!-- <div
+        ref="input"
+        class="input"
+        contenteditable="true"
+        @keypress.enter="handleKeyword($event)"
+        @input="updateInput($event)"
+      ></div> -->
+    </div>
   </header>
   <main class="content">
     <jobListingCardVue v-for="(item, index) in jobs" :key="index" :job="item" />
@@ -15,7 +39,24 @@ export default defineComponent({
   components: { jobListingCardVue },
   setup() {
     const jobs = ref(jobsData);
-    return { jobs };
+
+    const input = ref();
+
+    let keyword = ref("");
+
+    let selectfilters = ref<any[]>([]);
+
+    const updateInput = (e: any) => {
+      keyword.value = e.target.innerText;
+    };
+
+    const handleKeyword = (e: any) => {
+      selectfilters.value.push(keyword.value);
+      keyword.value = "";
+      // input.value.innerText = "";
+    };
+
+    return { jobs, handleKeyword, updateInput, selectfilters, input, keyword };
   },
 });
 </script>
